1f6f93faf702d550362488e3872cc3503764f265
zzz3230 <xus.util@gmail.com>
2024-09-09 23:51:23 +0700

    init

---
+++ Devs/main.cpp
@@ -0,0 +1,256 @@
+#include <iostream>
+#include <gmp.h>
+#include <any>
+
+namespace global_a{
+
+    void collision(){
+        
+    }
+}
+
+namespace global_b{
+    namespace ss
+    {
+        
+    } // namespace ss
+    
+    void collision(){
+
+    }
+}
+
+
+using namespace std;
+
+class Foo {
+public:
+    Foo(int val){
+        cout << "created " << val << endl;
+        field0 = val;
+    }
+
+    ~Foo(){
+        cout << "destroyed " << field0 << endl;
+    }
+
+    int& hidden_f();
+
+    const int& hidden_f_consted_alt() const;
+    const int& hidden_f_consted() const{
+        return field0;
+    }
+
+    int read_f(){
+        return field0;
+    }
+
+private:
+    
+    int arr[123];
+    int field0;
+};
+
+const int& Foo::hidden_f_consted_alt() const{
+    return field0;
+}
+
+int& Foo::hidden_f(){
+    return field0;
+}
+
+
+
+class Bar {
+public:
+
+    void setup(){
+        foo = new Foo(10);    
+    }
+
+    void dox(){
+        cout << foo->read_f(); 
+    }
+
+    const Foo& getit(){
+        return *foo;
+    }
+
+private:
+    Foo* foo;
+};
+
+
+template<typename T>
+int hello(T val){
+    val.hidden_f();
+    return 10;
+}
+
+
+int* get_val(){
+    //int x = 16;
+    //int* refs = &x;
+    //return refs;
+
+    return nullptr;
+}
+
+class Wallet{
+public:
+    Wallet(){
+
+    }
+
+    const int& get_amount() {
+        return amount;
+    }
+
+    void operator=(int value){
+        amount += value;
+    }
+
+    void operator%(string r){
+        
+    }
+
+private:
+    int amount = 0;
+};
+
+int main(){
+
+    Wallet* wal_ptr;
+    {
+        Wallet mywallet{};
+        cout << mywallet.get_amount() << endl;
+        mywallet = 1024;
+        cout << mywallet.get_amount() << endl;
+
+        wal_ptr = &mywallet;
+    }
+    
+    *wal_ptr = 7770000;
+    
+    int yyy = 123'456;
+
+    Foo(123);
+    
+    qsort(0, 0, 0, [](auto a, auto b) -> int {return a > b});
+
+    delete new Foo(123);
+    
+    cout <<  wal_ptr->get_amount() << endl;
+
+    Foo axar(2);
+        
+
+    //int val2 = *get_val();
+
+    //cerr << "aaa";
+
+    //int& uval = *static_cast<int*>(nullptr);
+
+    //*(int*)nullptr;
+
+
+    
+
+    //Foo& fref = fval;
+    //Foo fnoref = fval;
+    //Foo& fxa = fval;
+
+    Foo&& fxb = Foo(66);
+    
+    Foo fval(88);
+
+    cout << fxb.read_f() << endl;
+
+    cout << "Some code" << endl;
+
+    return 0;
+
+
+    //cout << "VAL: " << uval << endl;
+
+    //int&& val3 = get_val();
+
+    //val3 = 2;
+
+    //auto strx = ("%d" % 2);
+
+    void (*fn)();
+    {
+        int value = 123;
+        auto fna = { [=]() { cout << value; } };
+        fn;
+    }
+
+    return 0;
+    //any xr;
+    auto xr = Foo(777);
+    
+    hello(xr);
+
+    struct {
+        int field1;
+        string field2;
+    } test_struct {10, "abc"};
+
+    
+
+    cout << test_struct.field1 << test_struct.field2 << endl;
+
+    //any_cast<Foo>(xr).hidden_f() = 666;
+    //reinterpret_cast<Foo*>(&xr)->hidden_f() = 888;
+
+    
+
+
+    //int value = 10;
+
+    //auto lm = [=]{ cout << value; };
+
+ /*    Foo foo(10);
+    cout << foo.read_f() << endl;
+    int& reditable = foo.hidden_f();
+
+    reditable = 5;
+
+    cout << foo.read_f() << endl; */
+
+global_a::collision();
+global_b::collision();
+
+
+    using namespace global_a;
+    using namespace global_b;
+
+
+    Foo z(1);
+    for (size_t i = 0; i < 3; i++)
+    {
+        cout << "LOOP BEGIN" << endl;
+        auto zz = Foo(2 + i);
+    }
+
+
+    return 0;
+
+    Foo foo(22);
+    Bar bar;
+    bar.setup();
+    bar.dox();
+    const int& x = bar.getit().hidden_f_consted();
+    //x = 77;
+
+    bar.dox();
+
+    const int& refx = 10;
+
+    cout << rand();
+
+    
+
+    return 0;
+}
---
+++ Lab0/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <stdexcept>
+
+bool is_word(char c){
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+}
+
+
+void save_words_rate(const std::string& output_file_name, const std::map<std::string, int>& words_rate, int total_words_count){
+    std::fstream output_file(output_file_name, std::ios::out);
+
+    if(!output_file.good()){
+        throw std::runtime_error("unable to create output file");
+    }
+    
+    output_file << "word,count,rate\n";
+
+    std::vector<std::pair<std::string, int>> rate_list;
+    std::copy(words_rate.begin(), words_rate.end(), std::back_inserter<std::vector<std::pair<std::string, int>>>(rate_list));
+
+    std::sort(rate_list.begin(), rate_list.end(), [](auto a, auto b) { return a.second > b.second; });
+
+    for (auto &kw : rate_list)
+    {
+        output_file << kw.first << "," << kw.second << "," << (double)kw.second / total_words_count * 100 << "%\n";
+    }
+    
+    output_file.close();
+}
+
+void count_words_rate(const std::string& file_name, std::map<std::string, int>& out_words_rate, int& total_words_count){
+
+    std::fstream file(file_name, std::ios::in);
+    
+    if(!file.good()){
+        throw std::exception("input file not available");
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::stringstream word_builder;
+        for (size_t i = 0; i < line.length(); i++)
+        {
+            if((std::isdigit(line[i]) || is_word(line[i]))){
+                word_builder << line[i];
+            }
+            else{
+                auto word = word_builder.str();
+                if(word.length() > 0){
+                    out_words_rate[word]++;
+                    total_words_count++;
+                    word_builder.str("");
+                }
+            }
+        }
+
+        auto word = word_builder.str();
+        if(word.length() > 0){
+            out_words_rate[word]++;
+            total_words_count++;
+        }
+    }
+    file.close();
+}
+
+int main(int argc, char** argv){
+
+    if(argc != 3){
+        std::cout << "Usage: " << std::endl << "$ app.exe file_with_text.txt result.csv" << std::endl;
+        return 0;
+    }
+
+    try
+    {
+        int total_words_count = 0;
+        std::map<std::string, int> words_rate;
+        
+        count_words_rate(std::string(argv[1]), words_rate, total_words_count);
+        save_words_rate(std::string(argv[2]), words_rate, total_words_count);
+    }
+    catch (std::exception& ex)
+    {
+        std::cout << "Error:" << std::endl;
+        std::cout << ex.what() << std::endl;
+        return -1;
+    }
+    
+    return 0;
+}
+
+
